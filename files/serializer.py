e0b1f9d4477bbdc782e35e59435fefef430ec73a
Allan Cordeiro <allangdc@gmail.com>
2022-05-24 02:15:08 -0300

    Adjust path

--- files/serializer.py
+++ files/serializer.py
@@ -1,20 +1,21 @@
 from rest_framework import serializers
 from .models import Files
-from paths.serializer import PathSerializer
+from paths.serializer import PathSerializer, PathSerializerFolder
 import os
 
 
 class FileSerializer(serializers.ModelSerializer):
-    folder = PathSerializer(read_only=True, source="path")
+    folder = PathSerializerFolder(read_only=True, source="path")
     size = serializers.SerializerMethodField("get_size")
     name = serializers.SerializerMethodField("get_name")
 
     def get_size(self, obj: Files):
-        return str(obj.specs.size)
+        return obj.specs.size
 
     def get_name(self, obj: Files):
         return os.path.basename(obj.specs.name)
 
     class Meta:
         model = Files
-        fields = ["id", "folder", "size", "name", "description", "specs"]
+        fields = ["id", "folder", "size", "name", "description", "specs", "path"]
+        extra_kwargs = {'path': {'write_only': True}}
--- paths/models.py
+++ paths/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+import os
 
 # Create your models here.
 
 
 class Paths(models.Model):
     path = models.CharField(max_length=200, unique=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return self.path
+        if self.path != "/":
+            return os.path.join(str(self.parent), self.path)
+        return "/"
+
+
